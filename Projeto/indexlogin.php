10d6e6eb5dd84c39b31c1f828fc5170f6055f96f
KAIK777 <88007525+KAIK777@users.noreply.github.com>
2021-11-23 19:40:34 -0300

    Projeto Final - Atualização

--- Projeto/indexlogin.php
+++ Projeto/indexlogin.php
@@ -1,27 +1,25 @@
 <?php
-    include('config.php');
-    if(isset($_POST['acao'])){
-        $usuario = $_POST['txtemail'];
-        $senha = $_POST['txtsenha'];
-        $sql = $pdo->prepare("SELECT * FROM tb_usuarios WHERE usuario = ?");
-        $sql->execute([$usuario]);
-
-        if($sql->rowCount() == 1){
-            $info = $sql->fetch();
-            if(password_verify($senha, $info['senha'])){
-                $_SESSION['login'] = true;
-                $_SESSION['id'] = $info['id'];
-                $_SESSION['usuario'] = $info['usuario'];
-                header("Location: main.php");
-                die();
-            }else{
-                //Erro
-                echo '<div class="box_erro_login"><p><i class="fas fa-exclamation-circle"></i> Usuário ou senha incorretos!</p></div>';
-            }
-        }else{
-            //Erro
-            echo '<div class="box_erro_login"><p><i class="fas fa-exclamation-circle"></i> Usuário não encontrado.</p></div>';
-        }
+    if(empty($_POST['txtusuario']) || empty($_POST['txtsenha'])) {
+        header('Location: formLogin.php');
+        exit();
     }
-    //https://www.mundodaprogramacao.com.br/blog/back-end/sistema-de-login-e-logoff
+
+    $Vemail = $_POST['txtusuario'];
+    $Vsenha = $_POST['txtsenha'];
+
+    $username = "root";
+    $password = "";
+
+    $conn = new PDO('mysql:host=localhost;dbname=loja_site', $username, $password);
+
+    $consulta = $conn-> query("SELECT * FROM usuario WHERE usuario = '$Vemail' AND senha = md5('$Vsenha');");
+
+    if($consulta->rowCount() == 1) {
+	    header('Location: index.php');
+	    exit();
+    }
+    else {
+        header('Location: login.php');
+        exit();
+    }
 ?>
